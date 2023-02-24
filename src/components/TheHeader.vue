8c1699d39f2ca29c8e9932f85906ba8b3c0b8200
brianzzzasd <repuestobrian2@gmail.com>
2023-02-24 10:49:39 +0800

    chore: wip

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -275,10 +275,10 @@ const mobileMenuOpen = ref(false)
                         </div>
 
                         <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 -translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 -translate-y-1">
-                          <PopoverPanel class="absolute inset-x-0 top-full text-gray-500 sm:text-sm">
+                          <PopoverPanel class="absolute inset-x-0 top-full text-gray-500 sm:text-sm w-1/2">
                             <!-- Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow -->
 
-                            <div class="relative bg-white w-1/2">
+                            <div class="relative bg-white">
                               <div class="mx-auto max-w-7xl px-8 bg-gray-100">
                                 <div class="grid grid-cols-1 items-start gap-y-10 gap-x-8 pt-10 pb-12">
                                   <div class="grid grid-cols-2 gap-y-10 gap-x-8">
