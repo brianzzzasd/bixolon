1dd711fbca10a57f9d601c598699f735bba98abf
brianzzzasd <repuestobrian2@gmail.com>
2023-03-01 01:25:59 +0800

    chore: wip

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -236,39 +236,37 @@ const navigation = {
         </div>
       </div>
       <div class="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
-        <div class="flex space-x-6 md:order-2">
+        <div class="flex items-center space-x-6 md:order-2">
           <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
             <span class="sr-only">{{ item.name }}</span>
             <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
           </a>
-        </div>
-        <Menu as="div" class="relative inline-block text-left">
-          <div>
-            <MenuButton class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-              Options
-              <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-            </MenuButton>
-          </div>
+          <Menu as="div" class="relative inline-block text-left">
+            <div>
+              <MenuButton class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                CAD
+                <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+              </MenuButton>
+            </div>
 
-          <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div class="py-1">
-                <MenuItem v-slot="{ active }">
-                  <a href="#" class="group flex items-center px-4 py-2 text-sm" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700']">
-                    <PencilSquareIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                    Edit
-                  </a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <a href="#" class="group flex items-center px-4 py-2 text-sm" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700']">
-                    <DocumentDuplicateIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-                    Duplicate
-                  </a>
-                </MenuItem>
-              </div>
-            </MenuItems>
-          </transition>
-        </Menu>
+            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="absolute -top-24 bg-transparentz-10 mt-2 w-24 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" class="group flex items-center px-4 py-2 text-sm" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700']">
+                      USD
+                    </a>
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" class="group flex items-center px-4 py-2 text-sm" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700']">
+                      EUR
+                    </a>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
         <p class="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
           &copy; BIXOLON America, Inc. All rights reserved.
         </p>
